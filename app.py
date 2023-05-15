122a762ec2b22995663dc87246ce9f1586940bd3
Octavian Helm <octavian.helm@lean-coders.at>
2023-05-15 11:53:00 +0200

    Added hostname to output message.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "Hello Flask!"
+    return "Hello Flask from %s!" % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=3000)
